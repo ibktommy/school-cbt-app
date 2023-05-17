545d3ee05b175404501e272897a1a8a88de327fe
ibktommy <atomquartz@gmail.com>
2023-05-17 14:36:09 +0100

    Created the data-js-file and import it in the main js file

---
+++ data.js
@@ -0,0 +1,165 @@
+ export const testData = [
+	{
+		subject: "General Knowledge",
+		questions: [
+			{
+				id: 1,
+				question: "Which country is regarded as the 'Giant of Africa'?",
+				optionA: "Egypt",
+				optionB: "Kenya",
+				optionC: "Nigeria",
+				optionD: "South Africa",
+				correctAnswer: "Nigeria",
+			},
+
+			{
+				id: 2,
+				question: "What is the capital state of Russia?",
+				optionA: "Kremlin",
+				optionB: "Saint Petersburg",
+				optionC: "Kiev",
+				optionD: "Moscow",
+				correctAnswer: "Moscow",
+			},
+
+			{
+				id: 3,
+				question: "Who is the first man to step foot on the moon?",
+				optionA: "Yuri Gaganrin",
+				optionB: "Neil Armstrong",
+				optionC: "Elon Musk",
+				optionD: "Edwin Adrin",
+				correctAnswer: "Neil Armstrong",
+			},
+
+			{
+				id: 4,
+				question: "What year did World War 2 ended?",
+				optionA: "1945",
+				optionB: "1948",
+				optionC: "1942",
+				optionD: "1947",
+				correctAnswer: "1945",
+			},
+
+			{
+				id: 5,
+				question: "Which of this countries is a world nuclear superpower",
+				optionA: "South Africa",
+				optionB: "Russia",
+				optionC: "United Arab Emirates",
+				optionD: "Malaysia",
+				correctAnswer: "Russia",
+			},
+		],
+	},
+
+	{
+		subject: "Biology",
+		questions: [
+			{
+				id: 1,
+				question: "The father of Zoology is?",
+				optionA: "Aristotle",
+				optionB: "Linnaeus",
+				optionC: "Whittalker",
+				optionD: "John Ray",
+				correctAnswer: "Aristotle",
+			},
+
+			{
+				id: 2,
+				question: "Throughout the world, official soecies names are in?",
+				optionA: "Latin",
+				optionB: "Greek",
+				optionC: "French",
+				optionD: "English",
+				correctAnswer: "Latin",
+			},
+
+			{
+				id: 3,
+				question: "Trypanosomiasis is transmitted by?",
+				optionA: "Mosquitoes",
+				optionB: "Glossina",
+				optionC: "Husefly",
+				optionD: "All of the above",
+				correctAnswer: "Glossina",
+			},
+
+			{
+				id: 4,
+				question: "Who suggested that 'All plants are made of cells'?",
+				optionA: "Hooke",
+				optionB: "Schwann",
+				optionC: "Schneider",
+				optionD: "Virchow",
+				correctAnswer: "Schneider",
+			},
+
+			{
+				id: 5,
+				question: "The interaction of living and non-living things is termed?",
+				optionA: "Enviromental Biology",
+				optionB: "Ecology",
+				optionC: "Ecosystem",
+				optionD: "nature",
+				correctAnswer: "Ecosystem",
+			},
+		],
+	},
+	{
+		subject: "Mathematics",
+		questions: [
+			{
+				id: 1,
+				question: "What is the next Prime number after 13?",
+				optionA: "14",
+				optionB: "19",
+				optionC: "17",
+				optionD: "12",
+				correctAnswer: "17",
+			},
+
+			{
+				id: 2,
+				question: "1,8,15,22,29,36,... is?",
+				optionA: "G.P",
+				optionB: "Arithmetic Series",
+				optionC: "Geometric Series",
+				optionD: "A.P",
+				correctAnswer: "A.P",
+			},
+
+			{
+				id: 3,
+				question: "Every even integer is also a/an ?",
+				optionA: "Natural number",
+				optionB: "Rational number",
+				optionC: "Whole number",
+				optionD: "All of the above",
+				correctAnswer: "Rational number",
+			},
+
+			{
+				id: 4,
+				question: "The set {{1, 2, 3}, {4, 5}} has how many elements?",
+				optionA: "2",
+				optionB: "Infinite",
+				optionC: "1",
+				optionD: "5",
+				correctAnswer: "5",
+			},
+
+			{
+				id: 5,
+				question: "Solve 3 + 6 x ( 5 + 4 ) / 3 - 7",
+				optionA: "14",
+				optionB: "16",
+				optionC: "11",
+				optionD: "15",
+				correctAnswer: "14",
+			},
+		],
+	},
+];
